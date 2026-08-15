297c4d9c0ff81b62183bdbd577e331a812d8e162
shahdnaalwah234-droid <shahdnaalwah234@gmail.com>
2026-08-15 22:08:06 +0300

    Update database.ts

--- src/db/database.ts
+++ src/db/database.ts
@@ -6,7 +6,7 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  max: 20,
+  max: 8,
   idleTimeoutMillis: 30000,
 });
 
